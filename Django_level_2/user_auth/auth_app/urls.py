06a3ff1d0d63d6933fa3489f1acedd98688c0fab
Sadja18 <namanmishraec1045@gmail.com>
2020-07-14 14:15:05 +0530

    another commit

--- Django_level_2/user_auth/auth_app/urls.py
+++ Django_level_2/user_auth/auth_app/urls.py
@@ -5,5 +5,5 @@ app_name = 'auth_app'
 
 urlpatterns = [
                 path('register/', views.register, name='register'),
-                path('login/', views.login, name = 'login'),
+                path('user_login/', views.user_login, name = 'user_login'),
 ]
