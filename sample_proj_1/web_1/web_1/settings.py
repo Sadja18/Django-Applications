5e198a9ccd6781224ea126815a7169c453426d07
Sadja18 <namanmishraec1045@gmail.com>
2020-07-28 18:38:49 +0530

    Using django-bootstrap-static

--- sample_proj_1/web_1/web_1/settings.py
+++ sample_proj_1/web_1/web_1/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp_1',
+    'bootstrap',
+    'bootstrap4',
+    'fontawesome',
+    'mediumeditor',
 ]
 
 MIDDLEWARE = [
