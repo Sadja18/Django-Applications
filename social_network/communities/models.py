aa3cea3a28236e098a007f2956a3dc93a7353334
Sadja18 <namanmishraec1045@gmail.com>
2020-07-28 20:57:46 +0530

    After group template edition

--- social_network/communities/models.py
+++ social_network/communities/models.py
@@ -1,3 +1,47 @@
+# Communities models.py
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+import misaka
+
+from django.utils import reverse
+from django.contrib.auth import get_user_model
+from django import template
+
+User = get_user_model()
+
+register = template.Library()
+
+class Community(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='CommunityMember')
+
+    def __str__(self):
+        return self.name
+
+    """
+    method to save the community object created
+    """
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('communities:single', **kwargs={'slug':self.slug})
+
+    class Meta():
+        ordering = ['name']
+
+class CommunityMember(models.Model):
+    community = models.ForeignKey(Community, related_name='memberships',on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_communities', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta():
+        unique_together = ('community', 'user')
--- social_network/communities/views.py
+++ social_network/communities/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import(LoginRequiredMixin,PermissionRequiredMixin)
 
-# Create your views here.
+from django.urls import reverse
+
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, render
+
+from django.views import generic
+from communities.models import Community, CommunityMember
+from . import models
+
+class CreateCommunityView(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Community
+
+class SingleCommunity(generic.DetailView):
+    model = Community
+
+class ListCommunity(generic.ListView):
+    model = Community
